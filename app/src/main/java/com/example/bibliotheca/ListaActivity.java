dae06cd2957e991004bf985725924cd89a6f9f6d
KaiqueCapucho <KaiqueCapucho13@gmail.com>
2025-12-06 15:17:26 -0300

    Bibliotheca v0.1

---
+++ app/src/main/java/com/example/bibliotheca/ListaActivity.java
@@ -0,0 +1,51 @@
+package com.example.bibliotheca;
+
+import android.os.Bundle;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import com.google.android.material.navigation.NavigationView;
+
+public class ListaActivity extends AppCompatActivity {
+
+    private ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista);
+
+        DrawerLayout drawerLayout = findViewById(R.id.main);
+        NavigationView navigationView = findViewById(R.id.navView);
+
+        // Inicialize o ActionBarDrawerToggle uma vez
+        Toolbar toolbar = findViewById(R.id.toolbar); // Inicializa com a toolbar "fechada"
+        setSupportActionBar(toolbar);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.Open, R.string.Close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+            }
+        });
+    }
+
+}
