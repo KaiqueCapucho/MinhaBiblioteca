cdf7014ce36b630f6379b1764863fdef1828b076
KaiqueCapucho <KaiqueCapucho13@gmail.com>
2025-12-13 15:38:18 -0300

    Bibliotheca v0.4 (Correção do Spinner + contagem de Livros)

--- app/src/main/java/com/example/bibliotheca/ListaActivity.java
+++ app/src/main/java/com/example/bibliotheca/ListaActivity.java
@@ -8,73 +8,72 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
-
-import com.google.android.material.navigation.NavigationView;
-
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ListaActivity extends AppCompatActivity {
 
     private SQLiteDatabase bibliotecaBD;
     private ListView lstView;
+    private TextView txtCont;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
 
         DrawerLayout drawerLayout = findViewById(R.id.main);
-        NavigationView navigationView = findViewById(R.id.navView);
 
         BDHelper bdHelper = new BDHelper(this);
         bibliotecaBD = bdHelper.getReadableDatabase();
         lstView = findViewById(R.id.listView);
         Spinner spnCategoria = findViewById(R.id.spnCategoria);
-
+        txtCont = findViewById(R.id.txtCont);
         //Configura a toolbar e seus elementos (aka. Drawer do menu lateral)
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("");
+        Objects.requireNonNull(getSupportActionBar()).setTitle("");
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.Open, R.string.Close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         drawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
             @Override
-            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
-            }
-
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) { }
             @Override
-            public void onDrawerOpened(@NonNull View drawerView) {
-            }
-
+            public void onDrawerOpened(@NonNull View drawerView) { }
             @Override
-            public void onDrawerClosed(@NonNull View drawerView) {
-            }
-
+            public void onDrawerClosed(@NonNull View drawerView) { }
             @Override
-            public void onDrawerStateChanged(int newState) {
-            }
+            public void onDrawerStateChanged(int newState) { }
         });
 
         configSpnCategoria(spnCategoria, bibliotecaBD); //adiciona os elementos(categorias) do bd no spinner
-
         spnCategoria.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                String itemSel = adapterView.getSelectedItem().toString();
-                configListView(itemSel, bibliotecaBD);
+                String sql = "SELECT Livros._id, titulo_original, titulo_pt_br, GROUP_CONCAT(Autores.nome) AS autores, GROUP_CONCAT(categoria) AS categorias " +
+                        "FROM Livros JOIN Livros_Autores ON Livros._id = Livros_Autores.livro_id " +
+                        "JOIN Autores ON Autores._id = Livros_Autores.autor_id " +
+                        "JOIN Livros_Categorias ON Livros._id = Livros_Categorias.livro_id " +
+                        "JOIN Categorias ON Categorias._id = Livros_Categorias.categoria_id " +
+                        "WHERE Categorias.categoria = ? GROUP BY Livros._id ";
+                Cursor cur = bibliotecaBD.rawQuery(sql, new String[]{spnCategoria.getSelectedItem().toString()});
+                LivrosAdapter livroAdap = new LivrosAdapter(ListaActivity.this, cur);
+                lstView.setAdapter(livroAdap);
+                txtCont.setText("Total: " +cur.getCount());
             }
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) { } //Não faz nada
         });
     }
 
-    private void configSpnCategoria(Spinner spn, SQLiteDatabase bd){
+    private void configSpnCategoria(Spinner spnCategoria, SQLiteDatabase bd){
         ArrayList<String> arratCat = new ArrayList<>();
         try (Cursor cur = bd.rawQuery("SELECT categoria FROM Categorias ORDER BY categoria ASC", null)) {
             if (cur.moveToFirst()) {
@@ -89,20 +88,7 @@ public class ListaActivity extends AppCompatActivity {
         }
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, arratCat);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spn.setAdapter(adapter);
-    }
-    private void configListView(String sqlParam, SQLiteDatabase bd){
-        String sql = "SELECT Livros._id, titulo_original, titulo_pt_br, GROUP_CONCAT(Autores.nome) AS autores, GROUP_CONCAT(categoria) AS categorias " +
-                "FROM Livros JOIN Livros_Autores ON Livros._id = Livros_Autores.livro_id " +
-                "JOIN Autores ON Autores._id = Livros_Autores.autor_id " +
-                "JOIN Livros_Categorias ON Livros._id = Livros_Categorias.livro_id " +
-                "JOIN Categorias ON Categorias._id = Livros_Categorias.categoria_id " +
-                "WHERE Categorias.categoria = ? " +
-                "GROUP BY Livros._id ";
-
-        Cursor cur = bd.rawQuery(sql, new String[]{sqlParam});
-        LivrosAdapter livroAdap = new LivrosAdapter(ListaActivity.this, cur);
-        lstView.setAdapter(livroAdap);
+        spnCategoria.setAdapter(adapter);
     }
 
     @Override
