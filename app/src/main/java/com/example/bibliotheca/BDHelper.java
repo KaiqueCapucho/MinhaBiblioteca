82d62eaedb55b4b99eb6e854be6af1787fe78535
KaiqueCapucho <KaiqueCapucho13@gmail.com>
2026-01-08 14:20:04 -0300

    v1.2 (Modo Noturno + correção bd)

--- app/src/main/java/com/example/bibliotheca/BDHelper.java
+++ app/src/main/java/com/example/bibliotheca/BDHelper.java
@@ -16,38 +16,50 @@ import java.util.ArrayList;
 public class BDHelper extends SQLiteOpenHelper {
     private static final String NOMEBD = "Bibliotheca.db";
     private static final int VERBD = 1;
-    private Context context;
-
+    private final Context context;
 
     public BDHelper(Context context){
         super(context, NOMEBD, null, VERBD);
         this.context = context;
+        try {
+            createDatabase();
+        } catch (IOException e) {
+            Log.e("DBCopyError", "Erro ao copiar o banco de dados", e);
+        }
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        copiarBD();
+
     }
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) { }
 
-    private void copiarBD() {
-        File bd = new File(context.getDatabasePath(NOMEBD).getAbsolutePath());
-        if (!bd.exists()) {
-            try (InputStream inp = context.getAssets().open(NOMEBD);
-                 OutputStream out = new FileOutputStream(bd)) {
-                byte[] buffer = new byte[1024];
-                int length;
-                while ((length = inp.read(buffer)) > 0) {
-                    out.write(buffer, 0, length);
-                }
-                out.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
-                Log.e("DBCopyError", "Erro ao copiar o banco de dados", e);
-            }
+    private void createDatabase() throws IOException {
+        File dbFile = context.getDatabasePath(NOMEBD);
+        if (!dbFile.exists()) {
+
+            SQLiteDatabase db = getReadableDatabase();
+            db.close();
+            copyDatabase(dbFile.getPath());
         }
     }
+
+
+    private void copyDatabase(String outputName) throws IOException {
+        InputStream inputStream = context.getAssets().open(NOMEBD);
+        OutputStream outputStream = new FileOutputStream(outputName);
+
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) > 0) {
+            outputStream.write(buffer, 0, length);
+        }
+        outputStream.flush();
+        outputStream.close();
+        inputStream.close();
+    }
+
 
     @Override
     public SQLiteDatabase getReadableDatabase() {
